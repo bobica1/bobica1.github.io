6abb5c971bea1d69fd2c874f91c4dd2abf84e7f1
bobica1 <todorovskimilica7@gmail.com>
2025-10-14 15:46:54 +0200

    skolearbaid 14.10.2025.

---
+++ js-grunnleggended/script.js
@@ -0,0 +1,31 @@
+// Variabler
+
+let antalliv =100.8;
+
+// cont NAVN ="Milica";
+let navn  = "Milica";
+let alder = 17;
+
+const PI  = Math.PI;
+
+let arrayBilder=["bilde1.jpg"];
+
+//Skrive ut
+
+console.log("Mitt navn er"+ navn);
+console.log(`Mitt navn er ${navn} og jeg er ${alder} år gammel !`);
+
+
+console.table(arrayBilder);
+
+console.warm(PI);
+
+
+
+document.getElementById("utskrift").innerText ="Mitt navn er"+ navn + " og jeg er " + alder + "år gammel !";
+
+let alder2 =prompt("Hvor gammel er du?");
+a
+
+
+
